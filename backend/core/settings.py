00b9c2606b65b22c2d49c5696fc20758f2642b4e
ShabbirHussainCodes <shabbirtech110@gmail.com>
2026-06-25 15:23:57 +0530

    feat: PostgreSQL Supabase connected, migrations done

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -61,11 +61,13 @@ TEMPLATES = [
 WSGI_APPLICATION = 'core.wsgi.application'
 
 # Database
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+    )
 }
 
 # Password validation
